d87ff5810996f0e1122d1f7dcd108fc38d24de9e
Kamil Żak <84241685+Ghostie333@users.noreply.github.com>
2026-08-07 13:05:50 +0200

    Update ROOT_CA certificate in certificates.h
    Updated ROOT_CA certificate with new lines.

--- src/certificates.h
+++ src/certificates.h
@@ -3,6 +3,5 @@
 
 static const char ROOT_CA[] PROGMEM = R"RAW(-----BEGIN CERTIFICATE-----
 MIIFQzCCBCugAwIBAgIQdTf5Xe2jfGfIKW8z4gbR7zANBgkqhkiG9w0BAQoFADBHMQswCQYDVQQGEwJVUzEWMBQG
-... (pełna treść PEM, bez zmian, nowe linie wstawia cert)
 -----END CERTIFICATE-----
 )RAW";
